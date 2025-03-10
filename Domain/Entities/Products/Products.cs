2ed276cb69e526a5069fefa20b01cc94633aad02
aanandchhimpa <142562540+aanandchhimpa@users.noreply.github.com>
2025-03-10 21:17:46 +0530

    updates

---
+++ Domain/Entities/Products/Products.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities.Categories;
+using Domain.Entities.Multi_Tenant;
+
+namespace Domain.Entities.Products
+{
+    public class Product : BaseTenantEntity
+    {
+        public string Name { get; set; }
+        public string Slug { get; set; } // SEO-Friendly URL
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        public bool IsActive { get; set; } = true;
+        public decimal BasePrice { get; set; } // Default price before variants
+        public int StockQuantity { get; set; } // Overall stock
+
+        public virtual ICollection<ProductImage> Images { get; set; } = new List<ProductImage>();
+        public virtual ICollection<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
+        public virtual ICollection<ProductAttribute> Attributes { get; set; } = new List<ProductAttribute>();
+    }
+
+}
