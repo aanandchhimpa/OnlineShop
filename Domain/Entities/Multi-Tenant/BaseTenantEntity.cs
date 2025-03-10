6082abdedbfa5248506f8b6ff2e44430dd0f5f17
aanandchhimpa <142562540+aanandchhimpa@users.noreply.github.com>
2025-03-10 21:17:35 +0530

    updates

--- Domain/Entities/Multi-Tenant/BaseTenantEntity.cs
+++ Domain/Entities/Multi-Tenant/BaseTenantEntity.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Common;
+using Domain.Entities.Account;
 using Domain.Entities.Website;
 
 namespace Domain.Entities.Multi_Tenant
@@ -14,7 +15,13 @@ namespace Domain.Entities.Multi_Tenant
         public string Name { get; set; }
         public string Logo { get; set; }
         public string Domain { get; set; } // Custom domain for the business
-        public ICollection<WebsiteSettings> Settings { get; set; }
+        public bool IsActive { get; set; } = true; // Business status
+
+        public virtual ICollection<User> Users { get; set; }
+       // public virtual ICollection<Product> Products { get; set; }
+       // public virtual ICollection<Order> Orders { get; set; }
+        public virtual ICollection<WebsiteSettings> Settings { get; set; }
     }
+
 
 }
