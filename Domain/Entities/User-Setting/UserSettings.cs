f0ede243a24a11ab5546a7c09b7276154b282b86
aanandchhimpa <142562540+aanandchhimpa@users.noreply.github.com>
2025-03-12 20:54:38 +0530

    updates entites websitesettings and user settings

---
+++ Domain/Entities/User-Setting/UserSettings.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Common;
+using Domain.Entities.Account;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.User_Setting
+{
+    public class UserSettings : BaseAuditableEntity
+    {
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        public bool AllowEmailNotifications { get; set; } // Enable/disable email notifications
+
+        public bool AllowSmsNotifications { get; set; } // Enable/disable SMS notifications
+
+        public bool AllowPushNotifications { get; set; } // Enable/disable push notifications
+
+        public bool AllowOrderStatusUpdates { get; set; } // Notify about order updates
+
+        public bool AllowPromotionalEmails { get; set; } // Notify about offers & deals
+
+        public bool AllowWishlistAlerts { get; set; } // Notify when wishlist item price drops
+
+        public bool AllowReviewAlerts { get; set; } // Notify when someone replies to a review
+
+        public string PreferredLanguage { get; set; } // Preferred language (e.g., "en", "hi")
+
+        public bool DarkModeEnabled { get; set; } // UI preference for dark mode
+    }
+
+}
--- Domain/Entities/Website/WebsiteContent.cs
+++ Domain/Entities/Website/WebsiteContent.cs
@@ -4,11 +4,13 @@ namespace Domain.Entities.Website
 {
     public class WebsiteContent : BaseAuditableEntity
     {
-        public string Key { get; set; } // A unique key to identify the content (e.g., "AboutUs", "ContactUs", "PrivacyPolicy")
-        public string Title { get; set; } // Title of the page or content (e.g., "About Us")
+        public string Key { get; set; } // Unique identifier (e.g., "AboutUs", "ContactUs", "PrivacyPolicy")
+        public string Title { get; set; } // Page title (e.g., "About Us")
         public string Content { get; set; } // HTML or plain text content
-        public string MetaDescription { get; set; } // For SEO purposes
-        public string MetaKeywords { get; set; } // For SEO keywords
+        public string MetaDescription { get; set; } // SEO description
+        public string MetaKeywords { get; set; } // SEO keywords
+        public string PageType { get; set; } // Type of page (e.g., "Static", "LandingPage", "Blog")
     }
+
 
 }
